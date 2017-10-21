c4662d33e4750ee744c716c49e27962e33b5ca1b
chrisjxn <usjacksons@gmail.com>
2017-10-21 10:16:19 -0600

    commit changes

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { createStore } from 'redux';
 import reducer from './ducks/reducer';
 
-export default createStore(reducer); // When this function is invoked, it returns the redux store that keeps track of the state tree of your application along with the reducers that were passed in.
+let store = createStore(reducer);
+
+export default store; // When this function is invoked, it returns the redux store that keeps track of the state tree of your application along with the reducers that were passed in.
 // NOTE: If you have more than one reducer, use the function combineReducers from redux.
 
 
